733781ec8ea610a707e9ec5c2fcbbfed4d00001c
farleyy <surelyilikemen@gmail.com>
2026-08-20 18:53:45 -0500

    Initial DevKit setup

---
+++ Main.lua
@@ -0,0 +1,17 @@
+-- Main.lua
+
+local reference = "https://raw.githubusercontent.com/Farls-Xavier/DevKit/refs/heads/main"
+
+local function Load(path)
+    local source = game:HttpGet(reference .. path)
+    return loadstring(source)()
+end
+
+local Loaded = Load("/Loader.lua").load()
+
+-- Retrieve all the loaded stuff I needed ok!!!
+
+local Library = Loaded.Library
+local Explorer = Loaded.Explorer
+local Services = Loaded.Services
+local BlurEffect = Loaded.BlurEffect
