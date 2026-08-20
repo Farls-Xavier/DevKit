703ba20e3d7a4d5659d756a5ebcc2d15e3e7cd45
farleyy <surelyilikemen@gmail.com>
2026-08-20 23:07:26 +0000

    Create API module for loading Roblox version
    Implement API module to load Roblox version and API data.

--- API.lua
+++ API.lua
@@ -1,1 +1,28 @@
+local API = {}
 
+local Services = setmetatable({}, {
+    __index = function(_, service)
+        return game:GetService(service)
+    end
+})
+
+function API.Load(Status)
+
+    Status("Checking Roblox Version")
+
+    local robloxVersion = httpget("https://clientsettings.roblox.com/v2/client-version/WindowsStudio64/channel/LIVE"):match("(version%-[%w]+)")
+
+    if not robloxVersion then
+        Status("Failed to check Roblox's version.")
+        error("Failed to check Roblox's version.")
+    end
+
+    Status("Roblox version: " .. robloxVersion)
+    Status("Getting Roblox API...")
+
+    local rawAPI = httpget("http://setup.roblox.com/" .. robloxVersion .. "-API-Dump.json")
+    local api = Services.HttpService:JSONDecode(rawAPI)
+    
+end
+
+return API
