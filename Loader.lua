018d94947e231d6da371cf9cd2f1921a2f28df6a
farleyy <surelyilikemen@gmail.com>
2026-08-20 02:44:11 +0000

    Add UI for loader with status updates

--- Loader.lua
+++ Loader.lua
@@ -17,18 +17,76 @@ local function tableCount(tbl)
     return count
 end
 
+local function createLoaderUi()
+    local Loader = Instance.new("ScreenGui")
+    local Window = Instance.new("Frame")
+    local Title = Instance.new("TextLabel")
+    local Status = Instance.new("TextLabel")
+    local Window_UICorner = Instance.new("UICorner")
+
+    Loader.Name = "DevKitLoader"
+    Loader.Parent = game:GetService("CoreGui")
+
+    Window.Name = "Window"
+    Window.Parent = Loader
+    Window.BackgroundColor3 = Color3.fromRGB(11, 13, 16)
+    Window.BackgroundTransparency = 0.03
+    Window.BorderSizePixel = 0
+    Window.AnchorPoint = Vector2.new(0.5, 0.5)
+    Window.Position = UDim2.fromScale(0.5, 0.5)
+    Window.Size = UDim2.fromOffset(450, 300)
+
+    Title.Name = "Title"
+    Title.Parent = Window
+    Title.BackgroundTransparency = 1
+    Title.BorderSizePixel = 0
+    Title.Position = UDim2.new(0.277777791, 0, 0.266666681, 0)
+    Title.Size = UDim2.fromOffset(200, 13)
+    Title.Font = Enum.Font.SourceSansLight
+    Title.Text = "DevKit"
+    Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Title.TextSize = 18
+
+    Status.Name = "Status"
+    Status.Parent = Window
+    Status.BackgroundTransparency = 1
+    Status.BorderSizePixel = 0
+    Status.Position = UDim2.new(0.277777791, 0, 0.333333343, 0)
+    Status.Size = UDim2.fromOffset(200, 13)
+    Status.Font = Enum.Font.SourceSans
+    Status.Text = "Loading..."
+    Status.TextColor3 = Color3.fromRGB(230, 233, 239)
+    Status.TextSize = 12
+
+    Window_UICorner.CornerRadius = UDim.new(0, 1)
+    Window_UICorner.Parent = Window
+
+    return {
+        Gui = Loader,
+        Window = Window,
+        Title = Title,
+        Status = Status,
+    }
+end
+
 function Loader.load()
-    local count = table.getn(modules)
+    local count = tableCount(modules)
+    local ui = createLoaderUi()
 
     print("Loading", count, "modules.")
 
     local loaded = {}
 
     for library, path in pairs(modules) do
+        ui.Status.Text = "Loading " .. path
+
         local source = game:HttpGet(base .. path)
         loaded[library] = loadstring(source)()
+
         print("Loaded", path)
     end
+
+    ui.Gui:Destroy()
 
     print("Finished loading", count, "modules.")
 
